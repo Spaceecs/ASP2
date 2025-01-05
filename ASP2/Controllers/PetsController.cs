628c3ea34bd8946c640cfba9ecf7676f8679e960
Spaceecs <azaza2046goi@gmail.com>
2025-01-05 13:34:27 +0200

    Add project files.

---
+++ ASP2/Controllers/PetsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP2.Controllers
+{
+    public class PetsController : Controller
+    {
+        public IActionResult Dog()
+        {
+            return Content("Собака: Це вірний і дружній друг, який любить гуляти та грати.");
+        }
+
+        public IActionResult Cat()
+        {
+            return Content("Кішка: Незалежна, граційна та іноді примхлива, але завжди мила.");
+        }
+
+        public IActionResult Parrot()
+        {
+            return Content("Папуга: Барвистий і говіркий птах, який приносить багато радості.");
+        }
+    }
+}
---
+++ ASP2/Controllers/WildAnimalsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP2.Controllers
+{
+    public class WildAnimalsController : Controller
+    {
+        public IActionResult Lion()
+        {
+            return Content("Лев: Король савани, відомий своєю силою та граційністю.");
+        }
+
+        public IActionResult Wolf()
+        {
+            return Content("Вовк: Хижак, що живе в зграях. Символ свободи та дикої природи.");
+        }
+
+        public IActionResult Eagle()
+        {
+            return Content("Орел: Гордий птах, що ширяє високо в небі та символізує силу.");
+        }
+    }
+}
